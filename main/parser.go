8bec5c7ed2f9039903d8961e9b628705c6afd1f5
ncouret <nathanael.couret@gmail.com>
2016-11-16 13:39:25 +0100

    parser WIP

--- main/parser.go
+++ main/parser.go
@@ -1,20 +1,18 @@
 package main
 
 import (
+  "flag"
   "log"
   "bufio"
   "os"
+  "fmt"
   "regexp"
 )
 
-type Target struct {
-  name string
-  commands []string
-  dependencies []string
-}
-
-func parse(path string) {
-  lines, err = readFile(path)
+func parse(path string) ([]string) {
+  lines, _ := readFile(path)
+  rawTargets := parseRawTargets(lines)
+  return lines
 }
 
 func readFile(path string)([]string, error) {
@@ -26,13 +24,46 @@ func readFile(path string)([]string, error) {
     defer file.Close()
 
     scanner := bufio.NewScanner(file)
-    lines = make([]string,0,8)
-    for i := 0; scanner.Scan(); i++ {
-        lines[i] = scanner.Text()
+    lines := make([]string,0,8)
+    for  scanner.Scan() {
+        lines = append(lines,scanner.Text())
     }
 
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
     return lines, err
+}
+
+func parseRawTargets(lines []string)(*map[string][]string) {
+  rawTargets := make(map[string][]string)
+  var (
+    tar = regexp.MustCompile("(?P<target>[[:print:]]+): (?P<deps>[[:print:]]*)")
+    command = regexp.MustCompile("(?P<cmds>)")
+    )
+    for i:= range lines {
+      if(tar.MatchString(lines[i])) {
+        if(command.MatchString(lines[i+1])) {
+
+        }
+      }
+    }
+  return &rawTargets
+}
+
+func main() {
+  file := flag.String("path", "Makefile", "Specify the Makefile path.")
+  flag.Parse()
+  fmt.Println("Opening Makefile",*file)
+  lines := parse(*file)
+  for i := range lines {
+    fmt.Println(i, lines[i])
+  }
+
+  re := regexp.MustCompile("(?P<first>[a-zA-Z]+) (?P<last>[a-zA-Z]+)")
+	fmt.Println(re.MatchString("Alan Turing"))
+	fmt.Printf("%q\n", re.SubexpNames())
+	reversed := fmt.Sprintf("${%s} ${%s}", re.SubexpNames()[2], re.SubexpNames()[1])
+	fmt.Println(reversed)
+	fmt.Println(re.ReplaceAllString("Alan Turing", reversed))
 }
