ced13ce4ad1f13258ac474a1c4e70fb7ebb2d93a
Vincent Chenal <vince.chenal@gmail.com>
2016-11-20 14:17:58 +0100

    Corrected exported variables so that we know dmake everywhere (/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/vince/bin/play-1.4.2:/home/vince/bin/idea-IU-145.972.3/bin:/home/vince/scripts:/usr/lib/go/bin:/home/vince/dev/Go/bin)

--- script/taktuk_root.sh
+++ script/taktuk_root.sh
@@ -17,8 +17,8 @@ apt-get update
 #Install go
 wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 echo "GOPATH=/home/$user/Go" >> /etc/profile
+echo "export PATH=$PATH:$GOPATH/bin/" >> /etc/profile
 source /etc/profile
 
 #Install Apache thrift compile/install dependencies
