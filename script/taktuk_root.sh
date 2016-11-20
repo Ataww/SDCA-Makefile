f9f1b5e14fe85a718b9aa394da71019ebaad7b29
Vincent Chenal <vince.chenal@gmail.com>
2016-11-20 12:15:41 +0100

    Updated deployment scripts: execution rights and corrected conflict between libav-tools and ffmpeg

--- script/taktuk_root.sh
+++ script/taktuk_root.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#taktuk -l root -f $OAR_FILE_NODES broadcast exec [ /home/vchenal/taktuk_root.sh $USER $(id -u) ]
+#taktuk -l root -f $OAR_FILE_NODES broadcast exec [ /home/vchenal/SDCA-Makefile/script/taktuk_root.sh $USER $(id -u) ]
 
 user=$1
 user_id=$2
@@ -34,7 +34,11 @@ make install
 #Install git
 apt-get install -y git
 
+
+
 #Install application dependencies
+wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz
+tar xpvf ffmpeg-git-64bit-static.tar.xz -C /usr/local/bin --strip-components=1
 apt-get install -y blender imagemagick unzip libav-tools
 
 #Open server port
