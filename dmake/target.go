751ddecbc9118656ebb07633943e34796cd99f88
ncouret <nathanael.couret@gmail.com>
2016-11-23 22:24:13 +0100

    comments

--- dmake/target.go
+++ dmake/target.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+/*
+* Struct representing a Target in the makefile
+* id: target's name
+* lineCommand: Target's command
+* serverId: Id of the server the target is being assigned to
+* done: flag indicating whether the target has been executed
+* computing: flag indicating whether the current target is being computed
+* dependencies: slice containing pointers to the target's dependencies
+*/
 type Target struct {
 	id           string
 	lineCommand      string
@@ -13,6 +22,13 @@ type Target struct {
 	dependencies []*Target
 }
 
+/*
+* Create a new instance of Target
+* id: The target's name
+* _lineCommand: The target's command
+*
+* return: a pointer to the newly created Target
+*/
 func NewTarget(id string, _lineCommand string) *Target {
 	target := new(Target)
 	target.id = id
@@ -22,10 +38,18 @@ func NewTarget(id string, _lineCommand string) *Target {
 	return target
 }
 
+/*
+* Add a dependency to the calling target.
+* dependency: Pointer to the dependency to add.
+*/
 func (t *Target) Add_Dependency(dependency *Target) {
 	t.dependencies = append(t.dependencies, dependency)
 }
 
+/*
+* Pretty print the target at the given level (number of tabs)
+* level: level in the tree
+*/
 func (t Target) Print(level int) {
 	fmt.Print("\n")
 	for i := 0; i < level; i++ {
@@ -38,6 +62,10 @@ func (t Target) Print(level int) {
 	}
 }
 
+/*
+* Check if the target is ready to be computed.
+* A target is ready if it is neither being computed nor done and if all its dependencies have been computed already.
+*/
 func (t *Target) Is_Computable() bool {
 	if t.computing == true {
 		return false
@@ -55,6 +83,10 @@ func (t *Target) Is_Computable() bool {
 	}
 }
 
+/*
+* Recursively check if the current leaf is ready for computation. Return the next available target, or nil if none can be found.
+* return: a pointer to the next target available for computation.
+*/
 func (t *Target) Get_Leaf() *Target {
 	if t.Is_Computable() {
 		return t
